b990c43b53d73b2206cdc6ead2776ad5c75058fd
Hannah Winslow <jandhwinslow@gmail.com>
2020-11-20 17:13:54 -0600

    login and signup working with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,98 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:sermon_atlas/cubit/sermon_cubit.dart';
+// import 'package:sermon_atlas/data/sermon_repository.dart';
+// import 'package:sermon_atlas/pages/sermon_search_page.dart';
+// import 'package:sermon_atlas/pages/login.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       routes: <String, WidgetBuilder> {
+//         '/screen1': (BuildContext context) => new LoginPage(),
+//         '/screen2' : (BuildContext context) => new SermonSearchPage(),
+//       },
+//       title: 'Material App',
+//       home: BlocProvider(
+//         create: (context) => SermonCubit(FakeSermonRepository()),
+//         child: SermonSearchPage(),
+//       ),
+//       //home: LoginPage()
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sermon_atlas/cubit/sermon_cubit.dart';
-import 'package:sermon_atlas/data/sermon_repository.dart';
-import 'package:sermon_atlas/pages/sermon_search_page.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sermon_atlas/pages/dummy.dart';
 import 'package:sermon_atlas/pages/login.dart';
+import 'package:sermon_atlas/pages/signup.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(App());
+}
 
-class MyApp extends StatelessWidget {
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: <String, WidgetBuilder> {
-        '/screen1': (BuildContext context) => new LoginPage(),
-        '/screen2' : (BuildContext context) => new SermonSearchPage(),
-      },
-      title: 'Material App',
-      home: BlocProvider(
-        create: (context) => SermonCubit(FakeSermonRepository()),
-        child: SermonSearchPage(),
-      ),
-      //home: LoginPage()
-    );
+    if (_error) {
+    } else if (!_initialized) {
+    } else {
+      print('Good!!!------------------------------------');
+    }
+    return MaterialApp(routes: <String, WidgetBuilder>{
+      '/screen1': (BuildContext context) => new LoginPage(),
+      '/screen2': (BuildContext context) => new SignUpPage(),
+      '/screen3': (BuildContext context) => new DummyPage(),
+    }, title: 'Material App', home: LoginPage());
   }
+  // Widget build(BuildContext context) {
+  //   // Show error message if initialization failed
+  //   if(_error) {
+  //     //return SomethingWentWrong();
+  //   }
+
+  //   // Show a loader until FlutterFire is initialized
+  //   if (!_initialized) {
+  //     //return Loading();
+  //   }
+  //   print('HLLO0oooooooooooooooooooooooooooooooooo');
+
+  //   return LoginPage();
+  // }
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -4,8 +4,8 @@ import 'package:sermon_atlas/cubit/sermon_cubit.dart';
 import 'package:sermon_atlas/data/sermon_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key key, this.title}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  SignUpPage({Key key, this.title}) : super(key: key);
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -15,13 +15,14 @@ class LoginPage extends StatefulWidget {
   // always marked "final".
   final String title;
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignUpPageState extends State<SignUpPage> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +46,17 @@ class _LoginPageState extends State<LoginPage> {
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
-    final loginButton = Material(
+    final confirmPasswordField = TextField(
+      controller: _confirmPasswordController,
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Confirm Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final registerButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
       color: Color(0xff01A0C7),
@@ -53,37 +64,36 @@ class _LoginPageState extends State<LoginPage> {
         //minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () async {
+          // try {
+          //   UserCredential userCredential = await FirebaseAuth.instance
+          //       .signInWithEmailAndPassword(
+          //           email: _emailController.text,
+          //           password: _passwordController.text);
+          //   print(userCredential.user.email + " successfully signed in");
+
+          // } on FirebaseAuthException catch (e) {
+          //   if (e.code == 'user-not-found') {
+          //     print('No user found for that email.');
+          //   } else if (e.code == 'wrong-password') {
+          //     print('Wrong password provided for that user.');
+          //   }
+          // }
           try {
-            print('Trying firebase');
             UserCredential userCredential = await FirebaseAuth.instance
-                .signInWithEmailAndPassword(
+                .createUserWithEmailAndPassword(
                     email: _emailController.text,
-                    password: _passwordController.text);
-            print(userCredential.user.email + " successfully signed in");
-            Navigator.pushReplacementNamed(context, '/screen3');
+                    password: _confirmPasswordController.text);
+                    print('Successfully created account for ' + _emailController.text);
+                    Navigator.pushReplacementNamed(context, '/screen1');
           } on FirebaseAuthException catch (e) {
-            if (e.code == 'user-not-found') {
-              print('No user found for that email.');
-            } else if (e.code == 'wrong-password') {
-              print('Wrong password provided for that user.');
+            if (e.code == 'weak-password') {
+              print('The password provided is too weak.');
+            } else if (e.code == 'email-already-in-use') {
+              print('The account already exists for that email.');
             }
+          } catch (e) {
+            print(e);
           }
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-    final signupButton = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        //minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: ()  {
-          Navigator.pushReplacementNamed(context, '/screen2');
         },
         child: Text("Sign Up",
             textAlign: TextAlign.center,
@@ -113,11 +123,12 @@ class _LoginPageState extends State<LoginPage> {
                 emailField,
                 SizedBox(height: 25.0),
                 passwordField,
+                SizedBox(height: 25.0),
+                confirmPasswordField,
                 SizedBox(
                   height: 35.0,
                 ),
-                loginButton,
-                signupButton,
+                registerButton,
                 SizedBox(
                   height: 15.0,
                 ),
