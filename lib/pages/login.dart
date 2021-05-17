8f8d19fe4ffd7d78d24547ebf3049d59e83eb3fb
Hannah Winslow <jandhwinslow@gmail.com>
2021-05-16 19:09:31 -0500

    no changes, just testing connection to git since its been awhile

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -104,7 +104,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget emailField() {
+  Widget emailField() { 
     return TextField(
       controller: _emailController,
       obscureText: false,
